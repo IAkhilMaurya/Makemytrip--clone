dc33bde3398462eb5d1f11cb639d3be1e10efa37
Akhil Maurya <infoakm116@gmail.com>
2024-07-23 11:03:49 +0530

    Add files via upload

---
+++ backend/src/utils/sendOTP.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const router = express();
+
+router.use(express.json());
+
+const userController = require('../controllers/user.controller')
+
+ReadableStreamDefaultController.post('./sendOTP.js', );
+
+module.exports = router;
